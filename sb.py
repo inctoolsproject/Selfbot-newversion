1f4da4130bf34615c5c96e973f54f23b8fad9c91
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-21 19:42:21 +0700

    Update sb.py

--- sb.py
+++ sb.py
@@ -1,25 +1,25 @@
  # -*- coding: utf-8 -*-
-from thrift.transport import,TTransport,TSocket,THttpClient,TTransport,TZlibTransport
-from thrift.protocol import,TCompactProtocol,TMultiplexedProtocol,TProtocol
-from thrift.server import THttpServer TServer TProcessPoolServer
+from thrift transport import,TTransport,TSocket,THttpClient,TTransport,TZlibTransport
+from thrift protocol import,TCompactProtocol,TMultiplexedProtocol,TProtocol
+from thrift server import,THttpServer TServer TProcessPoolServer
 from linepy import *
-from akad.ttypes import *
+from akad ttypes import *
 from multiprocessing import Pool, Process
 from time import sleep
 from humanfriendly import format_timespan, format_size, format_number, format_length
 import pytz, datetime, pafy, time, timeit, random, sys, ast, re, os, json, subprocess, threading, string, codecs, requests, tweepy, ctypes, urllib, wikipedia
 from datetime import timedelta, date
 from datetime import datetime
-from urllib.parse import quote
+from urllib parse import quote
 from bs4 import BeautifulSoup
 import youtube_dl
 from zalgo_text import zalgo
 from threading import Thread,Event
 import wikipedia as wiki
-requests.packages.urllib3.disable_warnings()
+requests packages.urllib3.disable_warnings()
 from ttypes import LoginRequest
 import json, requests, LineService
-from thrift.transport import THttpClient
+from thrift transport import THttpClient
 
 botStart = time.time()
 cl = LINE("UqDG1qwjuufwlwI7kVmshsT96v1gNS5qzq1ja60hOlcUk/kePj7YvlBZwccfljjUbEdAQg3kMivW17QgR+13izCFCXKNEdwCxyUGESOzdwyOC22bmMJphdXhxPwsCFCXKNEdwCxyUGESOzdwyOC22bmMqSPhX/zBwLGUS=")
