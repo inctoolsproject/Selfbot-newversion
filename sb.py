8ef42ec698e1dd80ae9b83c61e1ce25a4c3e4bfb
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-21 20:11:43 +0700

    Update sb.py

--- sb.py
+++ sb.py
@@ -1,13 +1,13 @@
  # -*- coding: utf-8 -*-
-from thrift transport import,TTransport,TSocket,THttpClient,TTransport,TZlibTransport
-from thrift protocol import,TCompactProtocol,TMultiplexedProtocol,TProtocol
-from thrift server import THttpServer TServer TProcessPoolServer
-from linepy import *
-from akad ttypes import *
-from multiprocessing import Pool, Process
+from thrift.transport import, TSocket, THttpClient, TTransport, TZlibTransport
+from thrift.protocol import, TCompactProtocol, TMultiplexedProtocol, TProtocol
+from thrift.server import, THttpServer TServer, TProcessPoolServer
+from linepy.import *
+from akad.ttypes import *
+from multiprocessing.import Pool, Process
 from time import sleep
-from humanfriendly import format_timespan, format_size, format_number, format_length
-import pytz, datetime, pafy, time, timeit, random, sys, ast, re, os, json, subprocess, threading, string, codecs, requests, tweepy, ctypes, urllib, wikipedia
+from humanfriendly.import format_timespan, format_size, format_number, format_length
+from import pytz, pafy, time, timeit, random, sys, ast, re, os, json, subprocess, threading, string, codecs, requests, tweepy, ctypes, urllib, wikipedia
 from datetime import timedelta, date
 from datetime import datetime
 from urllib parse import quote
