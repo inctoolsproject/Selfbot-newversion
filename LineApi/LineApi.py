cd4d80762f308a3fdea12d769d587d0d638ca582
แทน <89076276+prataennaimjoi@users.noreply.github.com>
2021-09-21 13:45:30 +0700

    Update LineApi.py

--- LineApi/LineApi.py
+++ LineApi/LineApi.py
@@ -37,9 +37,9 @@ class LINE:
     self.authToken = self.Talk.authToken
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0', 
+              'X-Line-Application': 'IOSIPAD 7.14.0 iPhone OS 10.12.0' 
               'X-Line-Access': self.authToken, 
-              'User-Agent': 'Line/6.0.0 iPad4,1 9.0.2'
+              'User-Agent': 'Line/7.18.1 
    }
     self.Poll = Poll(self.authToken)
     self.channel = channel.Channel(self.authToken)
